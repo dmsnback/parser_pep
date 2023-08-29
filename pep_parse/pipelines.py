1a1262738b995b229e77e6c1b7f430d67f10dd91
Dmitry Titenkov <lt200711@yandex.ru>
2023-08-30 01:18:18 +0300

    Спринт 19 Ревью правки

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+import csv
 from datetime import datetime as dt
 from pathlib import Path
 
@@ -7,30 +9,32 @@ DATE_FORMAT = '%Y-%m-%d_%H-%M-%S'
 
 
 class PepParsePipeline:
-    counter_status = {}
-
     def open_spider(self, spider):
-        pass
+        self.counter_status = defaultdict(int)
 
     def process_item(self, item, spider):
         if item['status'] not in self.counter_status:
-            PepParsePipeline.counter_status[item['status']] = 1
+            self.counter_status[item['status']] = 1
         else:
-            PepParsePipeline.counter_status[item['status']] += 1
+            self.counter_status[item['status']] += 1
 
         return item
 
     def close_spider(self, spider):
-        date_time = str(dt.now().strftime(DATE_FORMAT))
+        date_time = dt.now().strftime(DATE_FORMAT)
         filename = 'status_summary_' + date_time + '.csv'
+        status_quantity = self.counter_status.items()
         total = sum(self.counter_status.values())
 
         with open(
                 BASE_DIR / 'results' / filename, 'w', encoding='utf-8'
         ) as file:
-            file.write('Статус,Количество\n')
+            write = csv.writer(file, dialect='unix')
 
-            for key, value in PepParsePipeline.counter_status.items():
-                file.write(f'{key}, {value}\n')
-
-            file.write(f'Total,{total}\n')
+            write.writerows(
+                [
+                    ['Статус', 'Количество'],
+                    *status_quantity,
+                    ['Total', total]
+                ]
+            )
